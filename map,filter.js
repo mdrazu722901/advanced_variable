cdb9629639ea770a3b0ae3aa303de320c8a2f215
mdraju <md raju>
2021-04-18 00:32:30 +0600

    done

---
+++ map,filter.js
@@ -0,0 +1,17 @@
+const number = [1, 3, 4, 2, 5, 7, 6, 0, 3, ];
+
+// const result = [];
+// for (let i = 0; i < number.length; i++) {
+//     const element = number[i];
+//     const squer = element * element;
+//     result.push(squer);
+// }
+// console.log(result);
+
+// const squer = number.map(x => x * x);
+// console.log(squer);
+
+// const squer = number.map(function(number, index, array) {
+//     return array;
+// })
+// console.log(array);
